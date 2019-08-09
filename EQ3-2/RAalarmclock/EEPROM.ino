804ee5b231bda849bfa60024c557ec76f8c270f8
Bob_work <musgravehill@gmail.com>
2019-08-09 20:38:47 +0300

    eq32 ra

--- EQ3-2/RAalarmclock/EEPROM.ino
+++ EQ3-2/RAalarmclock/EEPROM.ino
@@ -1,8 +1,9 @@
+
+///This will work for numbers between 0 & 65535.
 void EEPROM16_Write(uint8_t a, uint16_t b) {
   EEPROM.write(a, lowByte(b));
   EEPROM.write(a + 1, highByte(b));
 }
-
 uint16_t EEPROM16_Read(uint8_t a) {
   return word(EEPROM.read(a + 1), EEPROM.read(a));
 }
